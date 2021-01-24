2240e2d550276a3320716d4903824d46705add20
Asiat Kokaeva <chi_town@Asiats-MacBook-Pro.local>
2021-01-24 12:24:51 -0600

    commit four

--- src/classTask.java
+++ src/classTask.java
@@ -2,7 +2,7 @@ public class classTask {
     public static void main(String[] args) {
         System.out.println("Working on class task");
         System.out.println("Practice");
-
+        System.out.println("aslkdj");
 
     }
 
